1ec8b30302bea5e280a8c3216e2ec7660d18498e
Thaian Ramalho <80857637+thaianramalho@users.noreply.github.com>
2024-06-18 21:52:47 -0300

    atts

--- obj/Debug/net8.0-windows/LawForm.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/LawForm.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LawForm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a33d742cd0ec418c11769e0edd8612fcb95179b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6724f47008c58a6c97eacf331f946c544d51ea51")]
 [assembly: System.Reflection.AssemblyProductAttribute("LawForm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LawForm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
